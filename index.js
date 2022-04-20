a5fc4d25ed265d855aa59416c743c46d8f7c901b
AtsKotzen <atsaudio@gmail.com>
2022-04-20 02:04:21 -0300

    Mostrando ultimos blocos

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /* import moralis */
 const Moralis = require("moralis/node")
 const { serverUrl, appId , masterKey } = require("./config")
+const moment = require("moment")
+
+moment.now();
+
 
 const getBscPrice = async () => {
     await Moralis.start({ serverUrl, appId, masterKey })
@@ -9,20 +13,33 @@ const getBscPrice = async () => {
           address: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
           chain: "bsc",
         });
+        console.log("====================================")
         console.log("BSC Price: ", price)
     })
   }
+  // get the latest block on BSC
+const getLatestBlock = async ()=>{
+  const date = moment.now()
+  const options = {
+  chain: "bsc",
+  date: date,
+  };
+  const block = await Moralis.Web3API.native.getDateToBlock(options);
+  console.log("====================================")
+  console.log("Block: ", block)
+}
 
   // get block content on BSC
-const getBlockInfo = async() => {
-    const options = { chain: "bsc", block_number_or_hash: "17099778" };  
-    const transactions = await Moralis.Web3API.native.getBlock(options);
-    console.log(transactions)
-} 
+// const getBlockInfo = async() => {
+//     const options = { chain: "bsc", block_number_or_hash: "17099778" };  
+//     const transactions = await Moralis.Web3API.native.getBlock(options);
+//     console.log(transactions)
+// } 
 
   
   getBscPrice()
-  getBlockInfo()
+  //getBlockInfo()
+  getLatestBlock()
   
   
   
