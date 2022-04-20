79d6a5a7fe6a6c7c3043857acb322f0446923305
AtsKotzen <atsaudio@gmail.com>
2022-04-20 02:43:51 -0300

    Buscando informações do último bloco

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const Moralis = require("moralis/node")
 const { serverUrl, appId , masterKey } = require("./config")
 const moment = require("moment")
 
-moment.now();
 
 
 const getBscPrice = async () => {
     await Moralis.start({ serverUrl, appId, masterKey })
     .then(async()=>{
-        const price = await Moralis.Web3API.token.getTokenPrice({
+        let price = await Moralis.Web3API.token.getTokenPrice({
           address: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
           chain: "bsc",
         });
@@ -17,29 +16,32 @@ const getBscPrice = async () => {
         console.log("BSC Price: ", price)
     })
   }
-  // get the latest block on BSC
-const getLatestBlock = async ()=>{
-  const date = moment.now()
-  const options = {
+  
+  
+  // get the latest block number on BSC
+  const getLatestBlockInfo = async ()=>{  
+  let date = moment.now()
+  let options = {
   chain: "bsc",
   date: date,
   };
-  const block = await Moralis.Web3API.native.getDateToBlock(options);
+  let block = await Moralis.Web3API.native.getDateToBlock(options);
+  latestBlockNumber = JSON.stringify(block.block)
   console.log("====================================")
-  console.log("Block: ", block)
+  console.log("Block: ", latestBlockNumber)  
+  getBlockInfo(latestBlockNumber);
 }
 
   // get block content on BSC
-// const getBlockInfo = async() => {
-//     const options = { chain: "bsc", block_number_or_hash: "17099778" };  
-//     const transactions = await Moralis.Web3API.native.getBlock(options);
-//     console.log(transactions)
-// } 
+  const getBlockInfo = async(latestBlockNumber) => {
+  const options = { chain: "bsc", block_number_or_hash: latestBlockNumber };  
+  const transactions = await Moralis.Web3API.native.getBlock(options);
+  console.log(transactions)
+} 
 
   
-  getBscPrice()
-  //getBlockInfo()
-  getLatestBlock()
+getBscPrice()
+getLatestBlockInfo()
   
   
   
